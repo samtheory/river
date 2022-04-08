9a55f3e289ef70ee471a58889a6c0835ca02740e
sajjad Alizadeh <samycrip10@gmail.com>
2022-04-08 14:46:04 +0430

    Add Auto Route

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,22 @@ import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:line_icons/line_icon.dart';
-import 'package:objectbox/objectbox.dart';
-
-import 'utils/database/person.dart';
+import 'package:river/routes/router.gr.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final savedThemeMode = await AdaptiveTheme.getThemeMode();
-  
+
   runApp(RiverApp(savedThemeMode: savedThemeMode));
 }
 
+/// My [MaterialApp] + [AdaptiveThem]
 class RiverApp extends StatelessWidget {
   final AdaptiveThemeMode? savedThemeMode;
-  const RiverApp({
+
+  final _appRouter = AppRouter();
+
+  RiverApp({
     Key? key,
     this.savedThemeMode,
   }) : super(key: key);
@@ -35,37 +37,13 @@ class RiverApp extends StatelessWidget {
         colorScheme: const ColorScheme.dark().copyWith(secondary: Colors.amber),
       ),
       initial: AdaptiveThemeMode.system,
-      builder: (theme, darkTheme) => MaterialApp(
+      builder: (theme, darkTheme) => MaterialApp.router(
         debugShowCheckedModeBanner: false,
         title: 'Adaptive Theme Demo',
         theme: theme,
         darkTheme: darkTheme,
-        home: const Home(),
-      ),
-    );
-  }
-}
-
-class Home extends HookConsumerWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
- 
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('River'),
-        actions: [
-          IconButton(
-              onPressed: () async {
-                var currentThem = await AdaptiveTheme.getThemeMode();
-                currentThem == AdaptiveThemeMode.light
-                    ? AdaptiveTheme.of(context).setDark()
-                    : AdaptiveTheme.of(context).setLight();
-              },
-              icon: LineIcon.sun())
-        ],
+        routerDelegate: _appRouter.delegate(),
+        routeInformationParser: _appRouter.defaultRouteParser(),
       ),
     );
   }
